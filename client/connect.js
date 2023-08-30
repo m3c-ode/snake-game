b127100a345b081436895c1e7a0e1ad4f144596d
Maxime <maxime.marechalmccoy@gmail.com>
2023-08-30 12:41:41 -0700

    setup setupInput and handleUserInput

--- client/connect.js
+++ client/connect.js
@@ -11,7 +11,40 @@ const connect = function() {
   conn.on('connect', (message) => {
     console.log("🚀 ~ file: connect.js:12 ~ conn.on ~ message:", message);
     console.log('Client connected to server');
-    console.log("Name: ___");
+    // console.log("Name: ___");
+    // console.log("Move: up");
+
+    conn.write("Name: ___");
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 100);
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 100);
+    setTimeout(() => {
+      conn.write("Move: up");
+
+    }, 100);
+    setTimeout(() => {
+      conn.write("Move: up");
+
+    }, 100);
+    setTimeout(() => {
+      conn.write("Move: up");
+
+    }, 100);
+    conn.write("Move: up");
+    conn.write("Move: right");
+    conn.write("Move: right");
+    conn.write("Move: right");
+    conn.write("Move: right");
+    const id = setInterval(() => {
+      conn.write("Move: right");
+
+    }, 50);
+    setTimeout(() => {
+      clearInterval(id);
+    }, 5000);
   });
 
   conn.on("data", (data) => {
